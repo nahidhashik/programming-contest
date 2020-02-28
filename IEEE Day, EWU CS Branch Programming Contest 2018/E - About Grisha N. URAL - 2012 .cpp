7c6c38b1352d2f317a1d39a7d27a37d4b3efbb5b
NahidHasanAshik <nahidhasanashik99@yahoo.com>
2020-02-29 05:24:33 +0600

    IEEE Day, EWU CS Branch Programming Contest 2018

---
+++ IEEE Day, EWU CS Branch Programming Contest 2018/E - About Grisha N. URAL - 2012 .cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int problem,t;
+    t=t*60;
+
+    cin>>problem;
+
+    if(problem*45>=300)
+        cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+
+
+    return 0;
+}
---
+++ IEEE Day, EWU CS Branch Programming Contest 2018/F - Two Gangsters URAL - 1409 .cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int h,l;
+    cin>>h>>l;
+    int harry,larry;
+    harry=(h+l-h-1);
+    larry=(l+h-l-1);
+    cout<<harry<<endl<<larry<<endl;
+
+    return 0;
+}
