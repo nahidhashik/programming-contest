c9a85d905c001dadbb6262ca4eab24439c169b5c
NahidHasanAshik <nahidhasanashik99@yahoo.com>
2020-02-29 05:34:17 +0600

    EWUPSC Individual Team Practice Contest - 2

---
+++ EWUPSC Individual Team Practice Contest - 2/A+B Problem POJ - 1000.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+int k,l;
+
+cin>>k>>l;
+
+cout<<k+l<<endl;
+
+
+return 0;
+}
---
+++ EWUPSC Individual Team Practice Contest - 2/Meal Delivery AtCoder - 2694.cpp
@@ -0,0 +1,24 @@
+
+using namespace std;
+
+int main()
+{
+    int x,a,b;
+
+    cin>>x>>a>>b;
+
+    int A,B;
+
+    A=abs(x-a);
+    B=abs(x-b);
+
+
+    if(A<B) cout<<"A"<<endl;
+
+    else cout<<"B"<<endl;
+
+
+
+
+    return 0;
+}
